953385ec29094089309d811e3e25f087b80b771e
Litebo Moeketse <lmoeketse@outlook.com>
2022-08-04 17:42:35 +0200

    Modification on the app components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
+import React, { useState } from 'react';
 import './App.css';
-import BreadBottom from './components/BreadBottom';
-import Fruit from './components/Fruit';
-import Patty from './components/Patty';
-import Vegetable from './components/Vegetable';
-import BreadTop from './components/BreadTop';
 
-function App() {
-  return (
-    <div className="App">
-     <h1> Regular Burger</h1>
-    <div className="bread-top"> <BreadTop /> </div>
-    <div className="fruit"> <Fruit /> </div>
-    <div className="patty"> <Patty /> </div>
-    <div className="vegetable"> <Vegetable /> </div>
-    <div className="bread-bottom"> <BreadBottom /> </div>
+
+function Tomato(){
+  const [hidden, setHidden] = useState (false);
+  const tomatoStyle = {
+  width: "80%",
+  height: "12%",
+  background: "red",
+  margin: "2% auto",
+  borderradius: "10px",
+  };
+  return(
+    <div style={tomatoStyle}>
+      {!hidden && <button onClick={ () => setHidden(true)}>Tom</button>}
     </div>
   );
 }
 
+function Lettuce(){
+  const [hidden, setHidden] = useState(false);
+  const lettuceStyle = {
+  width: "80%",
+  height: "12%",
+  background: "green",
+  margin: "2% auto",
+  borderradius: "3px",
+  };
+  return (
+    <div style={lettuceStyle}>
+      {!hidden && <button onClick={ () => setHidden(true)}>Let</button>}
+    </div>
+
+  );
+}
+
+function App(){  
+//const [hidden, setHidden] = useState(false);
+  return (
+    <div className="App">
+    <h1> Regular Burger </h1>
+    <div className="bread-top"> </div>
+    <div> <Tomato /> </div>
+    <div className="patty"> </div>
+    <div> <Lettuce /> </div>
+    <div className="bread-bottom"> </div>
+    </div>
+  );
+}
 export default App;
