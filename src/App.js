6a35782d11190bfaea7dae628a62f40dc1210a9d
Litebo Moeketse <lmoeketse@outlook.com>
2022-08-14 22:42:53 +0200

    Modification on burger fillings

--- src/App.js
+++ src/App.js
@@ -1,67 +1,72 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
 function Tomato(){
-  const [hidden, setHidden] = useState (false);
-  const tomatoStyle = {
-  display: "inline",
-  color: "white",
-  width: "80%",
-  height: "50px",
-  background: "red",
-  margin: "2% auto",
-  borderRadius: "12px",
-  border: "12px solid transparent",
-  };
-  return(
-    <div>
-      {!hidden && <button style={tomatoStyle} onClick={ () => setHidden(true)}>Tomato</button>}
-    </div>
-  );
-}
+    const [tomato, setTomato] = useState (false);
+    const tomatoStyle = {
+    display: "inline",
+    color: "white",
+    width: "80%",
+    height: "50px",
+    background: "red",
+    margin: "2% auto",
+    borderRadius: "12px",
+    border: "12px solid transparent",
+    };
+    return(
+      <div>
+        {!tomato && <button style={tomatoStyle} onClick={ () => setTomato(true)}>Tomato</button>}
+      </div>
+    );
+  }
 
-function Lettuce(){
-  const [hidden, setHidden] = useState(false);
-  const lettuceStyle = {
-  display: "inline",
-  color: "white",
-  width: "80%",
-  height: "50px",
-  background: "green",
-  margin: "2% auto",
-  border: "12px solid transparent",
-  borderRadius: "12px",
-  };
-  return (
-    <div8>
-      {!hidden && <button style={lettuceStyle} onClick={ () => setHidden(true)}>Lettuce</button>}
-    </div8>
+  function Lettuce(){
+    const [lettuce, setLettuce] = useState(false);
+    const lettuceStyle = {
+    display: "inline",
+    color: "white",
+    width: "80%",
+    height: "50px",
+    background: "green",
+    margin: "2% auto",
+    border: "12px solid transparent",
+    borderRadius: "12px",
+    };
+    return (
+      <div8>
+        {!lettuce && <button style={lettuceStyle} onClick={ () => setLettuce(true)}>Lettuce</button>}
+      </div8>
+  
+    );
+  }
 
-  );
-}
+  function Patty() {
+    const [patty, setPatty] = useState (false);
+    const pattyStyle = {
+    display: "inline",
+    color: "white",
+    width: "80%",
+    height: "70px",
+    background: "linear-gradient(#7f3608, #702e05)",
+    margin: "2% auto",
+    borderRadius: "12px",
+    border: "12px solid transparent",
+    };
+    return(
+      <div>
+        {!patty && <button style={pattyStyle} onClick={ () => setPatty(true)}>Meat</button>}
+      </div>
+    ); 
+  }
 
-function Patty(){
-  const [hidden, setHidden] = useState (false);
-  const pattyStyle = {
-  display: "inline",
-  color: "white",
-  width: "80%",
-  height: "70px",
-  background: "linear-gradient(#7f3608, #702e05)",
-  margin: "2% auto",
-  borderRadius: "12px",
-  border: "12px solid transparent",
-  };
-  return(
-    <div>
-      {!hidden && <button style={pattyStyle} onClick={ () => setHidden(true)}>Meat</button>}
-    </div>
-  ); 
-}
-
-function App(){  
-//const [hidden, setHidden] = useState(false);
+  function AddPatty ({patty, setPatty}) {
+    return(
+      <div>
+        {<button onClick={Patty}>Add Patty</button>}
+      </div>
+    );
+  }
+  function App(){  
   return (
     <div className="App">
     <h1> Regular Burger </h1>
@@ -70,8 +75,9 @@ function App(){
     <div> <Patty /> </div>
     <div> <Lettuce /> </div>
     <div className="bread-bottom"> </div>
-    <div> <addTomato /> </div>
+    <div> <AddPatty /></div>    
     </div>
   );
 }
+
 export default App;
